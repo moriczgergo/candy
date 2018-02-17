548e41fa45f668f5dddc7c94c79e5f20b4acefb4
skiilaa <greg@trofix.co>
2018-02-17 18:41:47 +0100

    WS server.

---
+++ index.js
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+var chalk = require('chalk'); // console colors
+const WebSocket = require('ws'); // websocket
+
+var { getReference, getItems } = require("./lib/reffs");
+
+var wss = new WebSocket.Server({
+	port: process.env.CANDY_PORT || 4540
+});
+
+wss.on('connection', function connection(ws) {
+	ws.on('message', function incoming(message) {
+		var messageObj = JSON.parse(message);
+		switch (messageObj.type) {
+			case "reference":
+				var ref = getReference(messageObj.path);
+				if (typeof ref != "undefined") {
+					ws.send(JSON.stringify({
+						type: messageObj.type,
+						reference: ref
+					}));
+				} else {
+					ws.send(JSON.stringify({
+						type: messageObj.type,
+						error: 404
+					}));
+				}
+				break;
+			case "list":
+				var items = getItems(messageObj.path);
+				if (typeof items != "undefined") {
+					ws.send(JSON.stringify({
+						type: messageObj.type,
+						items: items
+					}));
+				} else {
+					ws.send(JSON.stringify({
+						type: messageObj.type,
+						error: 404
+					}));
+				}
+				break;
+			default:
+				ws.send("{}");
+				break;
+		}
+	});
+});
+
+console.log(chalk.cyan("Testing getItems:"));
+console.log(JSON.stringify(getItems("/")));
+console.log(chalk.cyan("Testing getReference:"));
+console.log(JSON.stringify(getReference("/helloWorld")));
